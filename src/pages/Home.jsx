7e6fd1f496e85c60ee32578abda5231309d9bbd6
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-24 21:55:57 -0500

    finished styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <section>
-      <h1>WELCOME, I&apos;M STUART </h1>
+      <h1 id='welcome'>WELCOME, I&apos;M STUART! </h1>
       <p>
         A Full Stack Web Developer with a passion for creating and developing
         web applications from ideas that have heart.
