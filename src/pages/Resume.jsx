90e3676f4442f1743bac0cbe188ec5fe428046ad
Stuart Osborn <stuartgosborn22@gmail.com>
2024-04-11 18:51:59 -0500

    updated embed element for mobile functionality

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,29 @@
-
-
 export default function Resume() {
   return (
     <section>
-      
-      <div style={{ width: '100%', height: '100vh' }}>
+      <div className="resumeContainer" >
+        <object
+          data="sgoOnlineReusme.pdf"
+          type="application/pdf"
+          frameBorder="0"
+          width="100%"
+          height="600px"
+          style={{ padding: "0px" }}
+        >
+          <embed
+            src="/sgoOnlineResume.pdf"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+          <p>
+            <a href="https://drive.google.com/file/d/1bqSUQb1_G4vchPrYFPidGC94z_pZVj3z/view?usp=drive_link" download="sgoOnlineResume.pdf">
+              Download PDF
+            </a>
+          </p>
 
-      <embed src='/sgoOnlineResume.pdf'  type="application/pdf" width="100%" height="100%"/>
+        
+        </object>
       </div>
     </section>
   );
